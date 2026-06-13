180074a93acec16f64bd591d1db7d2da8c4fb75c
mohan72 <mohan72@gmail.com>
2026-06-13 23:10:08 +0300

    Add files via upload

---
+++ keydefs.h
@@ -0,0 +1,13 @@
+#ifndef KEYDEFS_H
+#define KEYDEFS_H
+
+#define ESC     27
+#define TAB     9
+#define BKSPACE 127 //KEY_BACKSPACE
+#define ENTER   10  //KEY_ENTER
+#define F1      265 //KEY_F(1)
+#define CTRL_Q  17
+#define CTRL_S  19
+#define CTRL_W  23
+
+#endif
