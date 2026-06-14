bac83d3e639c5eed04e1b12069af826415b30185
mohan72 <mohan72@gmail.com>
2026-06-14 14:09:24 +0300

    Update keydefs.h

--- keydefs.h
+++ keydefs.h
@@ -6,7 +6,7 @@
 #define BKSPACE_A 127   //KEY_BACKSPACE
 #define BKSPACE_B 8     //KEY_BACKSPACE
 #define ENTER   10      //KEY_ENTER
-#define F1      265     //KEY_F(1)
+//#define F1      265     //KEY_F(1)
 #define CTRL_Q  17
 #define CTRL_S  19
 #define CTRL_W  23
